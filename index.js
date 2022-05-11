4135566ee9a7a3a29a357efd8175329b467587c2
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-05-10 20:50:41 -0600

    Completed core lab reqs

---
+++ index.js
@@ -0,0 +1,31 @@
+let message = document.querySelector("#message");
+
+const addMovie = event => {
+    event.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    document.querySelector("ul").appendChild(movie);
+    inputField.value = "";
+}
+
+const deleteMovie = event => {
+    event.target.parentNode.remove();
+    message.textContent = "Deleted!";
+}
+
+document.querySelector("form").addEventListener("submit", addMovie);
+
+const crossOffMovie = event => {
+    event.target.classList.toggle("checked");
+    (event.target.classList.contains("checked")) ? message.textContent = "Movie watched!" : message.textContent = "Movie added back!";
+}
